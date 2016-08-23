f44ee28bad1f54a96f3709e7621beaab9f8b628e
kourosh mirzaei <Koory.yellow@gmail.com>
2016-08-22 20:32:39 -0400

    Merge branch 'spotify-test' into gh-pages
    # Conflicts:
    #	css/application.css
    #	js/min/web-api-min.js
    #	js/web-api.js
    #	scss/.sass-cache/02346cb2c5c27f31357e4f1e2555b46e7f29ca76/_containers
    2.scssc
    #	scss/.sass-cache/02346cb2c5c27f31357e4f1e2555b46e7f29ca76/_containers
    3.scssc
    #	scss/.sass-cache/02346cb2c5c27f31357e4f1e2555b46e7f29ca76/_containers
    4.scssc
    #
    scss/.sass-cache/02346cb2c5c27f31357e4f1e2555b46e7f29ca76/_containers.sc
    ssc
    #	scss/.sass-cache/05ac6473b5dfa5574945b25eabda7b63f36a485d/_typograph
    2.scssc
    #	scss/layout/_containers.scss

--- js/web-api.js
+++ js/web-api.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
+    var retrieved = [];
 
 
+<<<<<<< HEAD
  $('#flickr-submit').click(function (evt) {
  	evt.preventDefault();
     // highlight the button
@@ -28,7 +30,90 @@ $(document).ready(function() {
     $.getJSON(flickerAPI, flickrOptions, displayPhotos);
 
      }); // end click
+=======
+>>>>>>> spotify-test
+
+
+    $('#flickr-submit').click(function(evt) {
+        evt.preventDefault();
+        var spotify_url = "https://api.spotify.com/v1/search";
+        var artist = $('#flickr-search').val().toLowerCase();
+        var spotifyOptions = {
+            q: artist,
+            type: "album"
+        };
+
+
+        function displayPhotos(data) {
+
+            
+            $.each(data.albums.items, function(i, photo) {
+                if (photo.images.length > 0) {
+                    var bandInfo = {
+                        "id": photo.id,
+                        "name": photo.name,
+                        "href": photo.href,
+                        "image": photo.images[0].url
+                    };
+                    retrieved.push(bandInfo);
+                }
+
+            }); // end each
+
+               function fetchDetails() {
+                $.each(retrieved, function(index, item) {
+                    var url = item.href;
+                    $.getJSON(url)
+                        .done(function(response) {
+                            retrieved[index].details = response;
+                        });
+                });
+            }
+            fetchDetails();
+
+
+            console.log(data);
+
+            var photoHTML = '';
+            //for (i = 0; i < retrieved.length; i++) {
+            $.each(retrieved, function(index, value) {
+                photoHTML += '<div class="photos">';
+                photoHTML += '<img src="' + value.image + '"></div>';
+            });
+            photoHTML += '</div>';
+            $('.gallery').html(photoHTML);
+        }
+        console.log(retrieved);
+        $.getJSON(spotify_url, spotifyOptions, displayPhotos);
+    }); // end click
+
+    // click on pics to open the light gallery
+
+
+
+
+    $('.gallery').on('click', 'img', function() {
+        var chosen_image = $(this).attr("src");
+        var chosen_url = $(this).attr("data-href");
+
+
+        $('.overlay-poster').attr("src", chosen_image);
+
+        $('#overlay').addClass('open');
+
+
+    });
+
+    $('#overlay').click(function() {
+        $('#overlay').removeClass('open');
+
+    });
+
+
+
+
+
+
+
 
 }); // end ready
-
-
