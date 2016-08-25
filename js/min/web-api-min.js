d0972d40f33397c228f22028ecba232bec3673ba
kourosh mirzaei <Koory.yellow@gmail.com>
2016-08-25 18:59:31 -0400

    mm

--- js/min/web-api-min.js
+++ js/min/web-api-min.js
@@ -1,1 +1,211 @@
-$(document).ready(function(){function a(){var a="";$.each(c,function(e,t){a+='<div class="photos">',a+='<img src="'+t.image+'" alt=" '+t.name+' ">',a+="</div>"}),$(".gallery").append(a)}function e(a){$.each(a,function(a,e){var t=e.href;$.getJSON(t).done(function(e){c[a].details=e})})}function t(a){$(".overlay-poster").attr("src",s[a].poster)}function n(a){$(".artist").html("<span>Artist :</span> "+s[a].name_band),$(".album").html("<span>album :</span> "+s[a].name_album),$(".release").html("<span>Release Date :</span> "+s[a].release_date)}function i(a){var e="";$.each(s[a].tracks_arr.items,function(a,t){e+="<tr><td>"+t.track_number+". "+t.name+"</td></tr>"}),$(".replace-table").html(e)}function r(){$.each(c,function(a,e){var t={index:a,name_album:e.name,name_band:e.details.artists[0].name,release_date:e.details.release_date,tracks_arr:e.details.tracks,poster:e.details.images[0].url};s.push(t)})}var s=[],l=10,c=[];$("#flickr-submit").click(function(t){function n(t){$.each(t.albums.items,function(a,e){if(e.images.length>0){var t={id:e.id,name:e.name,href:e.href,image:e.images[0].url};c.push(t)}}),e(c),a()}t.preventDefault(),$(".gallery").empty(),c=[];var i=$("#flickr-search").val().split(" ").join("+"),r="https://api.spotify.com/v1/search",s={q:i,type:"album",limit:l};$.getJSON(r,s,n)});var o;$(".gallery").on("click","div",function(){o=$(this).index(),r(),n(o),i(o),t(o),$("#overlay").addClass("open").css("display","flex")}),$("#overlay").click(function(){$("#overlay").removeClass("open")}),$(".left-arrow").click(function(a){a.stopPropagation(),o>0?(o-=1,n(o),i(o),t(o)):0===o&&(o=19,n(o),i(o),t(o))}),$(".right-arrow").click(function(a){a.stopPropagation(),18>=o?(o+=1,n(o),i(o),t(o)):19===o&&(o=0,n(o),i(o),t(o))})});
+$(document).ready(function() {
+
+    var newRetrived = [];
+    var limit = 10;
+    var retrieved = [];
+
+
+
+
+
+
+
+
+    $('#flickr-submit').click(function(evt) {
+        evt.preventDefault();
+        $('.gallery').empty();
+        retrieved = [];
+        var artist = $('#flickr-search').val().split(" ").join("+");
+
+
+        var spotify_url = "https://api.spotify.com/v1/search";
+
+
+        var spotifyOptions = {
+            q: artist,
+            type: "album",
+            limit: limit
+        };
+     
+
+        function displayPhotos(data) {
+
+            $.each(data.albums.items, function(i, photo) {
+                if (photo.images.length > 0) {
+                    var bandInfo = {
+                        "id": photo.id,
+                        "name": photo.name,
+                        "href": photo.href,
+                        "image": photo.images[0].url
+                    };
+                    retrieved.push(bandInfo);
+                }
+
+            }); // end each
+
+            fetchDetails(retrieved);
+
+            galleryBuilt();
+        }
+
+        $.getJSON(spotify_url, spotifyOptions, displayPhotos);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }); // end submit
+
+    //$('.gallery').empty();
+
+    ////////////// FUNCTIONS \\\\\\\\\\\\\\\
+
+    function galleryBuilt() {
+
+        var photoHTML = '';
+        $.each(retrieved, function(index, value) {
+            photoHTML += '<div class="photos">';
+            photoHTML += '<img src="' + value.image + '" alt=" ' + value.name + ' ">';
+            photoHTML += '</div>';
+        });
+
+        $('.gallery').append(photoHTML);
+    }
+
+
+    function fetchDetails(got) {
+        $.each(got, function(index, item) {
+            var url = item.href;
+            $.getJSON(url)
+                .done(function(response) {
+                    retrieved[index].details = response;
+                });
+        });
+    }
+
+    function rightPoster(rightIndex) {
+        $('.overlay-poster').attr("src", newRetrived[rightIndex].poster);
+    }
+
+
+    function albumInfo(rightIndex) {
+        $('.artist').html('<span>Artist :</span> ' + newRetrived[rightIndex].name_band);
+        $('.album').html('<span>album :</span> ' + newRetrived[rightIndex].name_album);
+        $('.release').html('<span>Release Date :</span> ' + newRetrived[rightIndex].release_date);
+    }
+    // function to make a table of tracks by the artist
+
+    function tableMaker(rightIndex) {
+        var table = '';
+        $.each(newRetrived[rightIndex].tracks_arr.items, function(index, item) {
+            table += '<tr><td>' + item.track_number + '. ' + item.name + '</td></tr>';
+
+        });
+        $('.replace-table').html(table);
+    }
+
+
+    // function to  make a new array with updated json
+
+    function usableInfo() {
+        $.each(retrieved, function(index, item) {
+            var newRetrivedObject = {
+                "index": index,
+                "name_album": item.name,
+                "name_band": item.details.artists["0"].name,
+                "release_date": item.details.release_date,
+                "tracks_arr": item.details.tracks,
+                "poster": item.details.images["0"].url
+            };
+            newRetrived.push(newRetrivedObject);
+        });
+    }
+
+
+
+
+
+
+
+
+    ////////////// click on pics to open the light gallery \\\\\\\\\\\
+    var clicked_index;
+
+
+    $('.gallery').on('click', 'div', function() {
+
+        clicked_index = $(this).index();
+        usableInfo();
+        albumInfo(clicked_index);
+        tableMaker(clicked_index);
+        rightPoster(clicked_index);
+
+        $('#overlay').addClass('open').css("display", "flex");
+
+
+    }); // end album click
+
+
+
+
+
+
+
+    $('#overlay').click(function() {
+
+        $('#overlay').removeClass('open');
+
+    });
+
+    ////////////// left and right arrows \\\\\\\\\\\\\\
+
+    $('.left-arrow').click(function(evt) {
+        evt.stopPropagation();
+
+
+        if (clicked_index > 0) {
+            clicked_index -= 1;
+            albumInfo(clicked_index);
+            tableMaker(clicked_index);
+            rightPoster(clicked_index);
+        } else if (clicked_index === 0) {
+            clicked_index = 19;
+            albumInfo(clicked_index);
+            tableMaker(clicked_index);
+            rightPoster(clicked_index);
+        }
+    });
+
+
+    $('.right-arrow').click(function(evt) {
+        evt.stopPropagation();
+        if (clicked_index <= 18) {
+            clicked_index += 1;
+            albumInfo(clicked_index);
+            tableMaker(clicked_index);
+            rightPoster(clicked_index);
+        } else if (clicked_index === 19) {
+            clicked_index = 0;
+            albumInfo(clicked_index);
+            tableMaker(clicked_index);
+            rightPoster(clicked_index);
+        }
+    });
+
+
+
+
+
+
+}); // end ready
+
+
