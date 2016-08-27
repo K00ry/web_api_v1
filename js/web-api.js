825ed9a69c96067db7af9da5be8326b3a3ee0214
Kourosh Mirzaei <koory.yellow@gmail.com>
2016-08-27 14:07:56 -0500

    Merge branch 'flicr-test'
    # Conflicts:
    #	config.codekit
    #	js/min/web-api-min.js
    #	js/web-api.js

--- js/web-api.js
+++ js/web-api.js
@@ -39,77 +39,24 @@ $(document).ready(function() {
             }); // end each
 
             fetchDetails(retrieved);
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-            console.log(retrieved);
-
-=======
->>>>>>> flicr-test
-=======
->>>>>>> flicr-test
-=======
->>>>>>> flicr-test
-=======
->>>>>>> flicr-test
             galleryBuilt();
         }
 
         $.getJSON(spotify_url, spotifyOptions, displayAlbums);
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> flicr-test
-=======
->>>>>>> flicr-test
 
 
-<<<<<<< HEAD
-    }); // end submit
-=======
-=======
->>>>>>> flicr-test
 
 
 
     }); // end submit
 
-
-    }); // end submit
-
-    ////////////// FUNCTIONS \\\\\\\\\\\\\\\
 
 
     ////////////// FUNCTIONS \\\\\\\\\\\\\\\
 
 
-    //// flickr call 
 
-    function FlickrCall(tag) {
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> flicr-test
-=======
->>>>>>> flicr-test
-        var flickr_url = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-        var flickrOptions = {
-            tags: tag,
-            tagmode: "any",
-            format: "json"
-            
-        };
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-        function displayPhotos(response) {
-
-=======
     //// flickr call 
 
     function FlickrCall(tag) {
@@ -124,38 +71,6 @@ $(document).ready(function() {
 
         function displayPhotos(response) {
 
->>>>>>> flicr-test
-            console.log(response.items);
-            var flickrPhotos = '';
-            $.each(response.items, function(index, item) {
-                 if (index === 5){
-                    return false;
-                }
-                flickrPhotos += '<div class="flickr-photos">';
-                flickrPhotos += '<img src=" ' + item.media.m + ' " alt=" ' + item.media.tags + ' ">';
-                flickrPhotos += '</div>';
-
-            }); //end each
-            $(".flickr-show").html(flickrPhotos);
-        } // end function
-        $.getJSON(flickr_url, flickrOptions, displayPhotos);
-    }
-<<<<<<< HEAD
->>>>>>> flicr-test
-
-
-<<<<<<< HEAD
- //-------////////////// FUNCTIONS \\\\\\\\\\\\\\\----------\\
-=======
->>>>>>> flicr-test
-=======
-
-
->>>>>>> flicr-test
-=======
-
-        function displayPhotos(response) {
-
             console.log(response.items);
             var flickrPhotos = '';
             $.each(response.items, function(index, item) {
@@ -173,29 +88,6 @@ $(document).ready(function() {
     }
 
 
->>>>>>> flicr-test
-=======
-
-        function displayPhotos(response) {
-
-            console.log(response.items);
-            var flickrPhotos = '';
-            $.each(response.items, function(index, item) {
-                 if (index === 5){
-                    return false;
-                }
-                flickrPhotos += '<div class="flickr-photos">';
-                flickrPhotos += '<img src=" ' + item.media.m + ' " alt=" ' + item.media.tags + ' ">';
-                flickrPhotos += '</div>';
-
-            }); //end each
-            $(".flickr-show").html(flickrPhotos);
-        } // end function
-        $.getJSON(flickr_url, flickrOptions, displayPhotos);
-    }
-
-
->>>>>>> flicr-test
 
     function galleryBuilt() {
 
